9ace02f3cf2773f987058f2ec0cefe9a725e8491
shlok-2003 <shlokprajapati2003@gmail.com>
2024-02-24 17:05:52 +0530

    Single source of icons

---
+++ src/icons/index.ts
@@ -0,0 +1,58 @@
+import {
+    FaStar,
+    FaChevronUp,
+    FaLinkedin,
+    FaTwitter,
+    FaInstagram,
+} from 'react-icons/fa';
+
+import {
+    VscVm,
+    VscAdd,
+    VscBook,
+    VscCallOutgoing,
+    VscAccount,
+    VscDashboard,
+    VscMortarBoard,
+    VscHistory,
+} from 'react-icons/vsc';
+
+import { FiUsers, FiArrowRight } from 'react-icons/fi';
+
+import { HiOutlineMenuAlt1 } from 'react-icons/hi';
+
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+
+import { BiWorld, BiArrowBack } from 'react-icons/bi';
+
+import { HiChatBubbleLeftRight } from 'react-icons/hi2';
+
+import { IoCall } from 'react-icons/io5';
+
+import { ImTree } from 'react-icons/im';
+
+export {
+    FiUsers,
+    FaStar,
+    FaChevronUp,
+    FiArrowRight,
+    FaLinkedin,
+    FaTwitter,
+    FaInstagram,
+    HiOutlineMenuAlt1,
+    HiChatBubbleLeftRight,
+    AiOutlineEye,
+    AiOutlineEyeInvisible,
+    BiWorld,
+    BiArrowBack,
+    IoCall,
+    ImTree,
+    VscVm,
+    VscAdd,
+    VscBook,
+    VscHistory,
+    VscAccount,
+    VscDashboard,
+    VscMortarBoard,
+    VscCallOutgoing,
+};
